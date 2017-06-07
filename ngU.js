39e06aef5a5636bae9e4238f08fd5dc57c684808
なの <hironaga1031@yahoo.co.jp>
2017-06-07 13:57:10 +0900

    ngU solved?

---
+++ ngU.js
@@ -0,0 +1,19 @@
+var x;
+var i;
+function reset(){
+    max = m;
+    i = Math.floor(max/2);
+}
+function bid() {
+return i; }
+function notify(k,r) {
+    if (r == 1) {
+        i--;
+    } else if (r == -1) {
+        i++;
+    } else { // r == 0
+    }
+}
+function id() {
+    return "3599586718";
+}
