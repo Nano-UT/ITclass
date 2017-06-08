bb7f82f6299999a137fba697962b77373d08b1ab
なの <hironaga1031@yahoo.co.jp>
2017-06-08 17:25:55 +0900

    problem solved^^

--- ngU.js
+++ ngU.js
@@ -1,11 +1,37 @@
 var x;
 var i;
+var MinAns;
+var MaxAns;
+var aveage;
+var befi;
 function reset(m){
-    max = m;
-    i = Math.floor(max/2);
+    MinAns = 1;
+    MaxAns = m;
+    var exp = 0;
+    average = 0;
+    for (var s = 1; s <= MaxAns; s++){
+      average += (1/s);
+    }
+    exp = (MaxAns - MinAns + 1) / average;
+    i = Math.round(exp);
+    befi = i;
 }
 function bid() {
-return i; }
+    if (i > befi){
+      MinAns = i;
+    }else{
+      MaxAns = i;
+    }
+    var exp = 0;
+    average = 0;
+    for (var s = MinAns; s <= MaxAns; s++){
+      average += (1/s);
+    }
+    exp = (MaxAns - MinAns + 1) / average;
+    i = Math.round(exp);
+    befi = i;
+    return i;
+}
 function notify(k,r) {
     if (r == 1) {
         i--;
